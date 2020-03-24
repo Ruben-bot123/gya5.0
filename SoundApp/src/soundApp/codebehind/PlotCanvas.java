9cf0c5ab81b9ceced905aa7632b859c384f51008
John <john.carlsson@rosendalsgymnasiet.se>
2020-03-24 14:19:00 +0100

    la till typvärde

--- SoundApp/src/soundApp/codebehind/PlotCanvas.java
+++ SoundApp/src/soundApp/codebehind/PlotCanvas.java
@@ -198,8 +198,7 @@ public class PlotCanvas extends JPanel implements Runnable, AudioStreamReceiver 
             }
             arr[j]=högstaton;
         }
-
-        högstaton = mostFrequent(arr,5);
+        högstaton = mostFrequent(arr,arr.length);
         //System.out.println(högstaton + " " + högstvolym);
 
         return högstaton;
